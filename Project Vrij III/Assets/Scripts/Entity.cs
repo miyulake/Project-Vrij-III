577d4eac33a19de4a35b6ef40ab40dd13ec2e9e0
M I Y U <jessewestwesterlakenjesse@outlook.com>
2025-08-15 15:35:43 +0200

    Added audio + Entity groundwork

--- Project Vrij III/Assets/Scripts/Entity.cs
+++ Project Vrij III/Assets/Scripts/Entity.cs
@@ -3,6 +3,19 @@ using UnityEngine;
 public class Entity : MonoBehaviour
 {
     private bool inHitstun = false;
+    private float hitstunTimer = 0f;
+
+    private void Update()
+    {
+        if (inHitstun) HandleHitstun();
+    }
+
+    private void HandleHitstun()
+    {
+        // get duration from the attack that the entity was hit with
+        hitstunTimer += Time.deltaTime;
+        //if (hitstunTimer >= hitstunDuration)
+    }
 
     public bool SetHitstunState(bool state) => inHitstun = state;
 }
