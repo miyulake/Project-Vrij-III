91f3568c2e50a42f7f1e6060d36c8d63e33f8b48
M I Y U <jessewestwesterlakenjesse@outlook.com>
2025-08-20 13:53:43 +0200

    Added blocking to animation hashes

--- Project Vrij III/Assets/Scripts/DataContainers/AnimationHashes.cs
+++ Project Vrij III/Assets/Scripts/DataContainers/AnimationHashes.cs
@@ -2,11 +2,12 @@ using UnityEngine;
 
 public static class AnimationHashes
 {
-    public static readonly int Idle           = Animator.StringToHash("Idle");
-    public static readonly int comboOne       = Animator.StringToHash("Combo_Attack_1");
-    public static readonly int comboTwo       = Animator.StringToHash("Combo_Attack_2");
-    public static readonly int comboThree     = Animator.StringToHash("Combo_Attack_3");
-    public static readonly int attackForward  = Animator.StringToHash("Attack_Forward");
-    public static readonly int attackDownward = Animator.StringToHash("Attack_Downward");
-    public static readonly int attackUpward   = Animator.StringToHash("Attack_Upward");
+    public static readonly int Idle            = Animator.StringToHash("Idle");
+    public static readonly int Block           = Animator.StringToHash("Block_Loop");
+    public static readonly int comboOne        = Animator.StringToHash("Combo_Attack_1");
+    public static readonly int comboTwo        = Animator.StringToHash("Combo_Attack_2");
+    public static readonly int comboThree      = Animator.StringToHash("Combo_Attack_3");
+    public static readonly int attackForward   = Animator.StringToHash("Attack_Forward");
+    public static readonly int attackDownward  = Animator.StringToHash("Attack_Downward");
+    public static readonly int attackUpward    = Animator.StringToHash("Attack_Upward");
 }
