c7ef8c350af4835648a5486bd19ef1e017177d2e
M I Y U <jessewestwesterlakenjesse@outlook.com>
2025-09-30 16:07:54 +0200

    Created game manager

--- Project Vrij III/Assets/InputReader.cs
+++ Project Vrij III/Assets/InputReader.cs
@@ -11,7 +11,7 @@ public class InputReader : MonoBehaviour
     public bool AttackForward => controls.Player.AttackForward.triggered;
     public bool AttackDownward => controls.Player.AttackDownward.triggered;
     public bool AttackUpward => controls.Player.AttackUpward.IsPressed();
-    public bool Blocking     => controls.Player.Block.IsPressed();
+    public bool Blocking => controls.Player.Block.IsPressed();
     public Vector2 Movement => controls.Player.Move.ReadValue<Vector2>();
 
     private void Awake() => controls = new Controls();
