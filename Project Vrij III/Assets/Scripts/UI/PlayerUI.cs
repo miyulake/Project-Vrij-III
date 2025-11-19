f88c22be37954e3ad57b8bb470875c935f6b5d5c
M I Y U <jessewestwesterlakenjesse@outlook.com>
2025-11-19 01:27:48 +0100

    Combo UI refactor
    Attack info does not work yet.

--- Project Vrij III/Assets/Scripts/UI/PlayerUI.cs
+++ Project Vrij III/Assets/Scripts/UI/PlayerUI.cs
@@ -1,15 +1,17 @@
 using UnityEngine;
 using TMPro;
+using Coffee.UIEffects;
 
 public class PlayerUI : MonoBehaviour
 {
     [SerializeField] private CombatUI m_PlayerOneUI;
     [SerializeField] private CombatUI m_PlayerTwoUI;
+    [SerializeField] private float m_DisplayDuration = 1f;
 
     private void Update()
     {
-        m_PlayerOneUI.SetComboUI();
-        m_PlayerTwoUI.SetComboUI();
+        m_PlayerOneUI.HandleComboUI(m_DisplayDuration);
+        m_PlayerTwoUI.HandleComboUI(m_DisplayDuration);
     }
 
     [System.Serializable]
@@ -18,16 +20,36 @@ public class PlayerUI : MonoBehaviour
         public StateManager opponentState;
         public Entity opponent;
         public TextMeshProUGUI attackText, comboText;
+        public UIEffectTweener effectTweener;
 
-        //public void SetAttackUI() => attackText.text = "";
-        public void SetComboUI()
+        private bool m_InCombo;
+        private float m_ComboTimer;
+
+        public void HandleComboUI(float timer)
         {
+            if (opponentState.CurrentState == EntityState.RECOVER) attackText.text = "Punish";
+            else if (opponentState.CurrentState == EntityState.ATTACK) attackText.text = "Counter";
+
             if (opponentState.CurrentState == EntityState.HITSTUN && opponent.ComboHits > 1)
             {
+                m_ComboTimer = 0;
+
                 comboText.text =
                     $"{opponent.ComboHits} Hit Combo \n {opponent.ComboDamage} Damage";
+
+                m_InCombo = true;
             }
-            else comboText.text = "";
+            else m_InCombo = false;
+
+            if (!m_InCombo)
+            {
+                m_ComboTimer += Time.deltaTime;
+                if (m_ComboTimer >= timer)
+                {
+                    attackText.text = "";
+                    comboText.text = "";
+                }
+            }
         }
     }
 }
