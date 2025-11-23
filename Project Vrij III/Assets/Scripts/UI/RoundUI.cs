8afd5822ff419818e78510e643cb9a555a1f36d5
M I Y U <jessewestwesterlakenjesse@outlook.com>
2025-11-23 14:00:55 +0100

    Updated to do list + added Round UI script

---
+++ Project Vrij III/Assets/Scripts/UI/RoundUI.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using TMPro;
+
+public class RoundUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI m_RoundTextMesh;
+    [SerializeField] private float m_DisplayDuration = 1;
+
+
+}
