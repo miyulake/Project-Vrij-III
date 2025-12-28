15f7fd9c400815cb3d37124ad30ff845a2e7c1cf
M I Y U <jessewestwesterlakenjesse@outlook.com>
2025-12-28 19:15:46 +0100

    Fixed hit shake consistency

--- Project Vrij III/Assets/Scripts/Entity/ShakeController.cs
+++ Project Vrij III/Assets/Scripts/Entity/ShakeController.cs
@@ -1,41 +1,48 @@
 using Game.Entities;
 using UnityEngine;
 
-public class ShakeController : EntityComponent, ITickable
+public class ShakeController : EntityComponent, ITickable, IResettable
 {
-    [SerializeField] private Transform shakeTarget;
-    private float shakeDuration;
-    private float shakeMagnitude;
-    private float shakeTimer;
-    private Vector3 originalPosition;
+    [SerializeField] private Transform m_ShakeTarget;
+    private float m_ShakeDuration;
+    private float m_ShakeMagnitude;
+    private float m_ShakeTimer;
+    private Vector3 m_OriginalPosition;
 
     public void Tick()
     {
-        if (shakeTarget == null) return;
-
-        if (shakeTimer > 0f)
+        if (m_ShakeTimer > 0f)
         {
-            shakeTimer -= Time.deltaTime;
+            m_ShakeTimer -= Time.deltaTime;
 
-            var durationSafe = Mathf.Max(shakeDuration, 0.0001f);
-            var progress = Mathf.Clamp01(1f - (shakeTimer / durationSafe));
-            var currentMagnitude = shakeMagnitude * Mathf.Sqrt(1f - progress);
+            var durationSafe = Mathf.Max(m_ShakeDuration, 0.0001f);
+            var progress = Mathf.Clamp01(1f - (m_ShakeTimer / durationSafe));
+            var currentMagnitude = m_ShakeMagnitude * Mathf.Sqrt(1f - progress);
             var offsetX = Random.Range(-1f, 1f) * currentMagnitude;
             var offsetY = Random.Range(-1f, 1f) * currentMagnitude;
             var shakeOffset = new Vector3(offsetX, offsetY, 0f);
 
-            shakeTarget.localPosition = originalPosition + shakeOffset;
-            if (shakeTimer <= 0f) shakeTarget.localPosition = originalPosition;
+            m_ShakeTarget.localPosition = m_OriginalPosition + shakeOffset;
+            if (m_ShakeTimer <= 0f) m_ShakeTarget.localPosition = m_OriginalPosition;
         }
+    }
+
+    public void Reset()
+    {
+        m_ShakeTimer = 0f;
+        m_ShakeDuration = 0f;
+        m_ShakeMagnitude = 0f;
+
+        m_OriginalPosition = Vector3.zero;
+        m_ShakeTarget.localPosition = Vector3.zero;
     }
 
     public void TriggerShake(float duration, float magnitude)
     {
-        if (shakeTarget == null) return;
+        if (m_ShakeTimer <= 0f) m_OriginalPosition = m_ShakeTarget.localPosition;
 
-        originalPosition = shakeTarget.localPosition;
-        shakeDuration = duration;
-        shakeTimer = duration;
-        shakeMagnitude = magnitude;
+        m_ShakeDuration = duration;
+        m_ShakeTimer = duration;
+        m_ShakeMagnitude = magnitude;
     }
 }
